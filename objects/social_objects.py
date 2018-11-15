e2adcf4e351a35c6dcb6a999572508c018070809
Caroline Hodge <caroline.n.hodge@gmail.com>
2018-11-15 17:18:23 -0500

    Added Races and Ballot Measures classes to social objects

--- objects/social_objects.py
+++ objects/social_objects.py
@@ -14,6 +14,7 @@ class ballots(db.Model):
     election_type = Column(Integer, db.ForeignKey('elections.uid')) # ADD THIS Look UP table
     sections = Column(Text) # THIS SHOULD BE A JSON STRING -- Possibly Re-Think This
 
+
     @property
     def __repr__(self):
         return '<Ballot %r>' % self.name
@@ -22,18 +23,33 @@ class candidates(db.Model):
     __tablename__ = 'candidates'
     cid = Column(Integer, primary_key=True)
     ballot_id = Column(Integer, db.ForeignKey('ballot.uid'))
-    party_id = Column(Integer, db.ForeignKey('parties.uid')) # Add this
+    # party_id = Column(Integer, db.ForeignKey('parties.uid')) # Add this
     status = Column(Boolean) # Does this need more?
     desc = Column(Text)
     candidate_name = Column(String(100), nullable=False)
+    bio = Column(Text, nullable=False)
+    policies = Column(Text, nullable=False)
 
     @property
     def __repr__(self):
         return '<Candidate %r>' % self.candidate_name
 
-class measures(db.Model):
+class measures(db.Model): #analogous to candidates
     __tablename__ = 'measures'
     mid = Column(Integer, primary_key=True)
     desc = Column(Text)
-    colloq_name = Column(String(150))
 
+class races(db.Model): 
+    __tablename__ = 'races'
+    rid = Column(Integer, primary_key=True)
+    desc = Column(Text) # governor, senator, etc.
+    duties = Column(Text) # what the governor does (duties/responsibilities)
+    cid = Column(Integer, db.ForeignKey('candidates.cid'))
+    bid = Column(Integer, db.ForeignKey('ballots.uid'))
+
+class ballot_measures(db.Model): # analagous to races
+    bmid = Column(Integer, primary_key=True)
+    desc = Column(Text) # Prop 1, Prop 2
+    colloq_name = Column(String(150))
+    mid = Column(Integer, db.ForeignKey('measures.mid'))
+    bid = Column(Integer, db.ForeignKey('ballots.uid'))
